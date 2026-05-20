2248bf00cd9515b7ee0bf4374e6ee44bb9b5cb72
I1HN <tabata27paodeqieijo@gmail.com>
2026-05-20 09:55:12 -0300

    fix: correção do ex1, erro na hora de linkar o arquivo js no html.

--- script1.js
+++ script1.js
@@ -1,6 +1,11 @@
-let personagem = {
-nome: prompt("digite o nome do personagem")
+let numeros = [];
+for (let i = 0; i < 5; i++) {
+    let numero = prompt("Digite um número:\n");
+    numeros[i] = Number(numero);
+}
+alert("os números escolhidos são:\n");
 
+for (let i = 0; i < 5; i++) {
 
-
+    alert(numeros[i]);
 }
