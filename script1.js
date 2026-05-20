18cf4ac8b51226172b9f06439835a93f9f408bdf
I1HN <tabata27paodeqieijo@gmail.com>
2026-05-20 10:48:44 -0300

    feito ex2 que cria um programa que colete dados do usuário

--- script1.js
+++ script1.js
@@ -1,14 +1,57 @@
-let personagens = {};
+let personagens = [];
 
 for (let i = 0; i < 3; i++) {
     let nome;
 
     while (true) {
         nome = prompt("coloque o nome do personagem [" + (i + 1) + "]:");
-            if (nome.trim() == "") {
-            alert("nome inválido!");
+
+        if (!nome || nome.trim() == "") {
+            alert("NOME INVÁLIDO");
         } else {
             break;
         }
     }
+
+    let obra;
+
+    while (true) {
+        obra = prompt("coloque o nome da obra (filme/série/jogo) [" + (i + 1) + "]:");
+
+        if (!obra || obra.trim() == "") {
+            alert("OBRA INVÁLIDA");
+        } else {
+            break;
+        }
+    }
+
+    let tipo;
+
+    while (true) {
+        tipo = prompt("coloque o tipo (filme, série ou jogo) [" + (i + 1) + "]:");
+
+        if (!tipo || tipo.trim() == "") {
+            alert("TIPO INVÁLIDO");
+        } else {
+            break;
+        }
+    }
+
+    personagens[i] = {
+        nomePersonagem: nome,
+        obraPersonagem: obra,
+        tipoPersonagem: tipo
+    };
 }
+
+let mensagem = "=== PERSONAGENS ===\n\n";
+
+for (let i = 0; i < personagens.length; i++) {
+    mensagem += "Personagem: " + personagens[i].nomePersonagem + "\n";
+    mensagem += "Obra: " + personagens[i].obraPersonagem + "\n";
+    mensagem += "Tipo: " + personagens[i].tipoPersonagem + "\n";
+    mensagem += "-------------------\n";
+}
+
+alert(mensagem);
+
