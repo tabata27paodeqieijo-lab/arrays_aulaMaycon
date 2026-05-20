176b09e25eef001e557a649108561346f0961ca7
I1HN <tabata27paodeqieijo@gmail.com>
2026-05-20 11:01:18 -0300

    feito ex3

--- script1.js
+++ script1.js
@@ -1,11 +1,48 @@
-let numeros = [];
-for (let i = 0; i < 5; i++) {
-    let numero = prompt("Digite um número:\n");
-    numeros[i] = Number(numero);
-}
-alert("os números escolhidos são:\n");
+let produtos = [];
+let soma = 0;
 
 for (let i = 0; i < 5; i++) {
+    let nome;
 
-    alert(numeros[i]);
+    while (true) {
+        nome = prompt("coloque o nome do produto [" + (i + 1) + "]:");
+
+        if (!nome || nome.trim() == "") {
+            alert("NOME INVÁLIDO");
+        } else {
+            break;
+        }
+    }
+
+    let preco;
+
+    while (true) {
+        preco = Number(prompt("coloque o preço do produto [" + (i + 1) + "]:"));
+
+        if (isNaN(preco) || preco <= 0) {
+            alert("PREÇO INVÁLIDO");
+        } else {
+            break;
+        }
+    }
+
+    produtos[i] = {
+        nomeProduto: nome,
+        precoProduto: preco
+    };
+
+    soma += preco;
 }
+
+let mensagem = "=== PRODUTOS ===\n\n";
+
+for (let i = 0; i < produtos.length; i++) {
+    mensagem += "Produto: " + produtos[i].nomeProduto + "\n";
+    mensagem += "Preço: R$ " + produtos[i].precoProduto.toFixed(2) + "\n";
+    mensagem += "-------------------\n";
+}
+
+mensagem += "\nTOTAL: R$ " + soma.toFixed(2);
+
+alert(mensagem);
+console.log(mensagem);
