027a7719d03ddafa30d89bbddf625c7fe38fa71e
I1HN <tabata27paodeqieijo@gmail.com>
2026-05-22 09:06:42 -0300

    feito ex5 que pede a criação de  um algoritmo que filtre produtos mais caros que R,00 ao apresentar ao usuário.

--- script1.js
+++ script1.js
@@ -23,9 +23,27 @@ for (let i = 1; i < produtos.length; i++) {
         maisBarato = produtos[i];
     }
 }
-let mensagem = "MAIS CARO: " + maisCaro.nomeProduto + " - R$ " + maisCaro.precoProduto + "\n";
-mensagem += "MAIS BARATO: " + maisBarato.nomeProduto + " - R$ " + maisBarato.precoProduto;
+
+let acimaDe15 = [];
+
+for (let i = 0; i < produtos.length; i++) {
+    if (produtos[i].precoProduto > 15) {
+        acimaDe15.push(produtos[i]);
+    }
+}
+
+let mensagem = "mais caro: " + maisCaro.nomeProduto + " - R$ " + maisCaro.precoProduto + "\n";
+mensagem += "mais barato: " + maisBarato.nomeProduto + " - R$ " + maisBarato.precoProduto + "\n\n";
+
+mensagem += "=== PRODUTOS ACIMA DE R$15 ===\n";
+
+if (acimaDe15.length == 0) {
+    mensagem += "nenhum produto acima de R$15.";
+} else {
+    for (let i = 0; i < acimaDe15.length; i++) {
+        mensagem += acimaDe15[i].nomeProduto + " - R$ " + acimaDe15[i].precoProduto + "\n";
+    }
+}
 
 alert(mensagem);
 console.log(mensagem);
-
