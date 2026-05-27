2f91cdd86f9a13651ccaefd3050c54d4e48059dd
I1HN <tabata27paodeqieijo@gmail.com>
2026-05-27 13:09:39 -0300

    main inicial

--- script1.js
+++ script1.js
@@ -1,58 +1,22 @@
-let produtos = [];
-
+let numeros = [];
 
 for (let i = 0; i < 5; i++) {
-    let nome;
-    let preco;
-
-       while (true) {
-        nome = prompt(`Digite o nome do produto [${i + 1}]:`);
-
-        if (!nome || nome.trim() === "") {
-            alert("nome inválido!");
-        } else {
-            break;
-        }
-    }
-
     while (true) {
-        preco = Number(prompt(`Digite o preço do produto [${i + 1}]:`));
+        let valor = Number(prompt("Digite o número [" + (i + 1) + "]:"));
 
-        if (isNaN(preco) || preco <= 0) {
-            alert("Preço inválido!");
+        if (isNaN(valor)) {
+            alert("Número inválido!");
         } else {
+            numeros[i] = valor;
             break;
         }
     }
-
-    produtos.push({
-        nomeProduto: nome,
-        precoProduto: preco
-    });
 }
 
+let mensagem = "=== NÚMEROS DIGITADOS ===\n\n";
 
-let maisCaro = produtos[0];
-let maisBarato = produtos[0];
-
-// Percorre o vetor para comparar os preços
-for (let i = 1; i < produtos.length; i++) {
-
-    if (produtos[i].precoProduto > maisCaro.precoProduto) {
-        maisCaro = produtos[i];
-    }
-
-    if (produtos[i].precoProduto < maisBarato.precoProduto) {
-        maisBarato = produtos[i];
-    }
+for (let i = 0; i < numeros.length; i++) {
+    mensagem += "Posição " + i + ": " + numeros[i] + "\n";
 }
-let mensagem = "=== RESULTADO ===\n\n";
-
-mensagem += "produto mais caro:\n";
-mensagem += `${maisCaro.nomeProduto} - R$ ${maisCaro.precoProduto.toFixed(2)}\n\n`;
-
-mensagem += "produto mais barato:\n";
-mensagem += `${maisBarato.nomeProduto} - R$ ${maisBarato.precoProduto.toFixed(2)}`;
 
 alert(mensagem);
-
